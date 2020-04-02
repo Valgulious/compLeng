cf3081a0edf49e412a39d58505b3be73e8342798
Valgulious <dmitryfedorenkof@gmail.com>
2020-04-02 22:22:25 +1000

    refactoring

--- test.py
+++ test.py
@@ -16,37 +16,6 @@ stop_words.extend(
 	['что', 'это', 'так', 'вот', 'быть', 'как', 'в', '—', 'к', 'на', ',', '«', '»', '.', 'т.д', 'т.п', '!', ':'])
 
 
-# document = open(r"test.txt").read()
-#
-#
-# def preprocess(document):
-# 	sentences = nltk.sent_tokenize(document)
-#
-# 	# стоп-слова которые мы считать не будем
-# 	stop_words = stopwords.words('russian')
-# 	print(stop_words)
-# 	# расширим набор дополнительным списком слов
-# 	stop_words.extend(
-# 		['что', 'это', 'так', 'вот', 'быть', 'как', 'в', '—', 'к', 'на', ',', '«', '»', '.', 'т.д', 'т.п', '!'])
-#
-# 	words = sum(1 for sent in sentences
-# 				for word in nltk.word_tokenize(sent)
-# 				if word not in stop_words
-# 				)
-# 	cnt = 0
-# 	for sent in sentences:
-# 		cnt += 1
-# 		wrds = nltk.word_tokenize(sent)
-# 		wrds_without = [word for word in wrds if not word in stop_words]
-# 		print(sent)
-# 		print(wrds)
-# 		print(wrds_without)
-# 	print('ИТОГ------------:')
-# 	return (len(sentences), words)
-#
-#
-# print(preprocess(document))
-
 class Text:
 
 	def __init__(self, _filename, _percent):
@@ -113,7 +82,7 @@ class Text:
 			referat += sent.sentence + "\n"
 
 		referat_file.write(referat)
-		print("Зареферированный текст находится в файле " + referat_filename)
+		return referat_filename
 
 
 class Sentence:
@@ -139,4 +108,4 @@ percent = input()
 text = Text(filename, int(percent))
 text.scored_sentences()
 text.sorted_sentences_by_score()
-text.referat()
+print("Зареферированный текст находится в файле " + text.referat())
